e568290255f403695174b7bccb6d5ac067e7d47a
Danish Mukhtar <danish241194@gmail.com>
2020-04-08 14:59:27 +0530

    changes

--- src/monitoring/main.py
+++ src/monitoring/main.py
@@ -3,6 +3,7 @@ import random
 import json
 app = Flask(__name__)
 
+machineagent_test={}
 
 servers = ["127.0.0.1:11000","127.0.0.1:11001","127.0.0.1:11002","127.0.0.1:11003","127.0.0.1:11004"];
 
@@ -19,7 +20,15 @@ def get_load():
 #     load["ip2:port2"] =random.randrange(100,1500,10)
 #     load["ip3:port3"] =random.randrange(100,1500,10)
 #     return "OK"
+@app.route('/monitoring/update', methods=['GET', 'POST'])
+def update():
+    content = request.json
+    machineagent_test[content["ip"]]=content["number"]
+    return machineagent_test
 
+@app.route("/monitoring/show")
+def show_test():
+	return machineagent_test
 
 if __name__ == "__main__":        # on running python app.py
     app.run(host="0.0.0.0",port=5050) 
--- src/server life cycle manager/code/machineagent.py
+++ src/server life cycle manager/code/machineagent.py
@@ -1,5 +1,9 @@
 import requests
+import sys
+a=1
 import time
-while(True):
-	res = requests.get('http://172.17.0.1:5050/monitoring/updateload')
+while True:
+	data = {"ip":sys.argv[1],"number":a}
+	a+=1
+	res = requests.post('http://172.17.0.1:5050/monitoring/update', json=data)
 	time.sleep(5)
